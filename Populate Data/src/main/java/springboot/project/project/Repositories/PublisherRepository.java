13e641537db4d9c17468cd6634611cfd62133282
AdnanKasmani1 <adnankasmani1@gmail.com>
2018-09-22 17:19:43 +0500

    Populate JPA DATA

---
+++ Populate Data/src/main/java/springboot/project/project/Repositories/PublisherRepository.java
@@ -0,0 +1,4 @@
+package springboot.project.project.Repositories;
+
+public interface PublisherRepository {
+}
---
+++ Populate Data/src/main/java/springboot/project/project/model/Publisher.java
@@ -0,0 +1,4 @@
+package springboot.project.project.model;
+
+public class Publisher {
+}
